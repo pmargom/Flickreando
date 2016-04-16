dfef03202838f87f5d96d5d30a5b55b02ba2495c
Pedro Martín <pmargom@gmail.com>
2016-04-16 15:35:17 +0100

    Changing the PageViewController transition style to scroll

--- Flickreando/PhotoPageViewController.swift
+++ Flickreando/PhotoPageViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class PhotoPageViewController: UIPageViewController {
 
+    override init(transitionStyle style: UIPageViewControllerTransitionStyle, navigationOrientation: UIPageViewControllerNavigationOrientation, options: [String : AnyObject]?) {
+        super.init(transitionStyle: .Scroll, navigationOrientation: .Horizontal, options: options)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
